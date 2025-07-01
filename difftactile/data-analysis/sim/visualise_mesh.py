6b1807aa913484573c441a0e04a4077c24e6fa1b
Piotr Blaszyk <piopio555888@gmail.com>
2025-07-01 20:58:16 +0100

    refactor data analysis code

--- difftactile/data-analysis/sim/visualise_mesh.py
+++ difftactile/data-analysis/sim/visualise_mesh.py
@@ -1,99 +1,67 @@
 import numpy as np
 import open3d as o3d
 import pickle
-from collections import Counter
 
 from difftactile.main.constants import *
 
-with open(SYSTEM_PARAMS.files.gmsh_mesh, 'rb') as f:
-    mesh_data = pickle.load(f)
+class VisualiseMesh:
+    def __init__(self):
+        self.load_data()
 
-print('hello')
-print(mesh_data['node_coordinates'].shape)
-print(mesh_data['all_tetrahedra'].shape)
+    def load_data(self):
+        with open(SYSTEM_PARAMS.files.gmsh_mesh, 'rb') as f:
+            self.mesh_data = pickle.load(f)
+        with open(SYSTEM_PARAMS.files.deformed_node_coordinates.format(SYSTEM_PARAMS.visualise_mesh.frame_number), 'rb') as f:
+            self.point_coordinates = pickle.load(f)
+        print(f'ts={SYSTEM_PARAMS.visualise_mesh.frame_number}; num nan: {np.sum(np.isnan(self.point_coordinates))}')
+    
+    def use_dome_surface_points(self):
+        self.point_coordinates = self.point_coordinates[self.mesh_data['dome_surface_node_tags']]
 
-with open(SYSTEM_PARAMS.files.deformed_node_coordinates.format(SYSTEM_PARAMS.visualise_mesh.frame_number), 'rb') as f:
-    deformed_node_coordinates = pickle.load(f)
-print(f'ts={SYSTEM_PARAMS.visualise_mesh.frame_number}; num nan: {np.sum(np.isnan(deformed_node_coordinates))}')
+    def remove_nans_and_filter_on_material(self):
+        nan_nodes = np.where(np.any(np.isnan(self.point_coordinates), axis=1))[0]
+        valid_nodes = np.setdiff1d(np.arange(len(self.point_coordinates)), nan_nodes)
+        old_to_new_idx = np.full(len(self.point_coordinates), -1)
+        old_to_new_idx[valid_nodes] = np.arange(len(valid_nodes))
+        self.point_coordinates = self.point_coordinates[valid_nodes]
+        all_tetrahedra_new_idx = np.array([[old_to_new_idx[i] for i in tetra] for tetra in self.mesh_data['all_tetrahedra']])
+        good_tetrahedra = []
+        for tetra in all_tetrahedra_new_idx:
+            if np.all(tetra != -1):
+                good_tetrahedra.append(tetra)
+        good_tetrahedra = np.array(good_tetrahedra)
+        self.tetrahedra = good_tetrahedra
 
-nan_nodes = np.where(np.any(np.isnan(deformed_node_coordinates), axis=1))[0]
+    def visualise_point_cloud(self):
+        _min = np.min(self.point_coordinates, axis=0)
+        _max = np.max(self.point_coordinates, axis=0)
+        diff = _max-_min
 
-# Filter out NaN nodes and create mapping
-valid_nodes = np.setdiff1d(np.arange(len(deformed_node_coordinates)), nan_nodes)
-old_to_new_idx = np.full(len(deformed_node_coordinates), -1)  # Initialize with -1
-old_to_new_idx[valid_nodes] = np.arange(len(valid_nodes))  # Map old indices to new indices
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(self.point_coordinates)
+        axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=np.max(diff)/2, origin=[0, 0, 0])
+        o3d.visualization.draw_geometries([pcd, axes])
 
-# Filter deformed_node_coordinates
-deformed_node_coordinates = deformed_node_coordinates[valid_nodes]
+    def visualise_mesh(self):
+        tetrahedra_decomposed = []
+        for tetra in self.tetrahedra:
+            a, b, c, d = tetra
+            tetrahedra_decomposed.append([a, b, c])
+            tetrahedra_decomposed.append([a, b, d])
+            tetrahedra_decomposed.append([a, c, d])
+            tetrahedra_decomposed.append([b, c, d])
+        tetrahedra_decomposed = np.array(tetrahedra_decomposed)
 
-# Convert all tetrahedra to new indices
-all_tetrahedra_new_idx = np.array([[old_to_new_idx[i] for i in tetra] for tetra in mesh_data['all_tetrahedra']])
+        mesh = o3d.geometry.TriangleMesh()
+        mesh.vertices = o3d.utility.Vector3dVector(self.point_coordinates)
+        mesh.triangles = o3d.utility.Vector3iVector(tetrahedra_decomposed)
+        mesh.compute_vertex_normals()
+        mesh = mesh.remove_duplicated_triangles()
+        o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
 
-good_tetrahedra = []
-for tetra in all_tetrahedra_new_idx:
-    tetra_node_labels = mesh_data['node_labels'][tetra]
-    gel_count = np.sum(tetra_node_labels[:, mesh_data['group_to_idx'].gel])
-    shell_count = np.sum(tetra_node_labels[:, mesh_data['group_to_idx'].shell])
-    is_gel = gel_count == 4 and shell_count <= 3
-    if not is_gel:
-        if np.any(tetra == -1):
-            continue
-        a, b, c, d = tetra
-        good_tetrahedra.append([a, b, c])
-        good_tetrahedra.append([a, b, d])
-        good_tetrahedra.append([a, c, d])
-        good_tetrahedra.append([b, c, d])
-good_tetrahedra = np.array(good_tetrahedra)
-print(len(good_tetrahedra) // 4)
 
-# deformed_node_coordinates = deformed_node_coordinates[1:]
-# good_tetrahedra -= 1
-
-if False:
-    with open(SYSTEM_PARAMS.files.vitactip_interp_idx_flat, 'rb') as f:
-        interp_idx_flat = pickle.load(f)
-
-    with open(SYSTEM_PARAMS.files.vitactip_cam_3d_nodes, 'rb') as f:
-        cam_3d_nodes = pickle.load(f)
-
-    with open(SYSTEM_PARAMS.files.tactile_sensor_f2v, 'rb') as f:
-        tetrahedra_indices = pickle.load(f)
-    tetrahedra_indices = tetrahedra_indices.astype(int)
-
-if False:
-    counter = Counter(interp_idx_flat)
-    for elem, count in counter.most_common():
-        print(f"{elem}: {count}")
-
-    # Remove duplicates
-    interp_idx_flat = np.unique(interp_idx_flat)
-
-    print(f'num of 3d marker points (after de-duplication): {interp_idx_flat.shape[0]}')
-
-    with open(SYSTEM_PARAMS.files.vitactip_surface_id, 'rb') as f:
-        surface_id_np = pickle.load(f)
-
-    surface_nodes = points[surface_id_np]
-    marker_nodes = surface_nodes[interp_idx_flat]
-    marker_nodes[:, 1] = 1.0
-    surface_nodes[:, 1] = 2.0
-    # Print mean values along each axis
-    print(f'Mean values of marker_nodes:')
-    print(f'X-axis mean: {np.mean(marker_nodes[:, 0]):.4f}')
-    print(f'Y-axis mean: {np.mean(marker_nodes[:, 1]):.4f}')
-    print(f'Z-axis mean: {np.mean(marker_nodes[:, 2]):.4f}')
-    # all_nodes[surface_id_np][np.unique(interp_idx_flat)]
-
-if False:
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(deformed_node_coordinates)
-    # axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
-    o3d.visualization.draw_geometries([pcd])
-
-if True:
-    mesh = o3d.geometry.TriangleMesh()
-    mesh.vertices = o3d.utility.Vector3dVector(deformed_node_coordinates)
-    mesh.triangles = o3d.utility.Vector3iVector(good_tetrahedra)
-    mesh.compute_vertex_normals()
-    mesh = mesh.remove_duplicated_triangles()
-    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+if __name__ == '__main__':
+    visualise_mesh = VisualiseMesh()
+    visualise_mesh.use_dome_surface_points()
+    visualise_mesh.visualise_point_cloud()
+    
