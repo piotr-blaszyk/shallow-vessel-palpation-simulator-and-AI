adapt the GNN to be run locally on MacOS cpu; runs but produces wrong results (ROC curve is wrong)

difftactile/cnn/dataset.py
@@ -1326,21 +1326,21 @@ class MyDataset(torch.utils.data.Dataset):
         labels = MyDataset.downscale(self.k, labels)
         return images[self.clip_len // 2, :, :]
 
-    @staticmethod
-    def generate_markers_image(h, w, points, points_mask):
-        if points.shape[-1] == 0:
-            return np.zeros((points.shape[0], h, w), dtype=np.uint8)
-        images = np.zeros((points.shape[0], h, w), dtype=np.uint8)
-        marker_masks = np.zeros((points.shape[0], h, w), dtype=np.uint8)
-        for t in range(points.shape[0]):
-            filtered_points = []
-            for i, point in enumerate(points[t]):
-                if points_mask[t, i]:
-                    filtered_points.append(point)
-                    MyDataset.draw_point(images, t, point)
-            markers_mask = SyntheticImageGenerator.compute_mask(h, w, filtered_points)
-            marker_masks[t, :, :] = markers_mask
-        return images, marker_masks
+    # @staticmethod
+    # def generate_markers_image(h, w, points, points_mask):
+    #     if points.shape[-1] == 0:
+    #         return np.zeros((points.shape[0], h, w), dtype=np.uint8)
+    #     images = np.zeros((points.shape[0], h, w), dtype=np.uint8)
+    #     marker_masks = np.zeros((points.shape[0], h, w), dtype=np.uint8)
+    #     for t in range(points.shape[0]):
+    #         filtered_points = []
+    #         for i, point in enumerate(points[t]):
+    #             if points_mask[t, i]:
+    #                 filtered_points.append(point)
+    #                 MyDataset.draw_point(images, t, point)
+    #         markers_mask = SyntheticImageGenerator.compute_mask(h, w, filtered_points)
+    #         marker_masks[t, :, :] = markers_mask
+    #     return images, marker_masks
 
     @staticmethod
     def draw_point(images, t, point, n=8, intensity=(255, 255, 255)):

difftactile/cnn/gnn.py
@@ -567,7 +567,7 @@ class GNN(pl.LightningModule):
         return self.get_accumulator(shape=(), dtype=torch.int32)
     
     def get_accumulator(self, shape, dtype):
-        return {k: torch.zeros(shape, dtype=dtype, device='cuda:0') for k in self.stages_str}
+        return {k: torch.zeros(shape, dtype=dtype, device='cpu') for k in self.stages_str}
     
     def init_accumulators(self):
         self.area_pred_acc = self.get_iou_accumulator()
@@ -853,7 +853,7 @@ def evaluate_and_plot_roc():
     if True:
         model = GNN()
         model.load_state_dict(torch.load(SYSTEM_PARAMS.files.final_segmentation_model_gnn))
-        model.eval()
+        # model.eval()
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = model.to(device)
 
@@ -876,7 +876,7 @@ def evaluate_and_plot_roc():
         stats = all_stats[target_difficulty]
         train_dataset.set_stats(stats)
         train_dataset.set_difficulty_level(target_difficulty)
-        train_dataset.eval()
+        # train_dataset.eval()
         data_loader = DataLoader(
             train_dataset,
             batch_size=16,

difftactile/main/synthetic_image_generator.py
@@ -1,4 +1,5 @@
-import cv2
+if False:
+    import cv2
 import numpy as np
 from scipy.spatial import Delaunay
 from shapely.geometry import MultiLineString, Polygon